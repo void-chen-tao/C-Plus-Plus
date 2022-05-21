02b755ae8ae7df078676caa5808ee2394ab3f94c
陈 涛 <1053949792@qq.com>
2022-05-21 18:55:00 +0800

    note of object and class in C++

---
+++ Cpp_Class/build/Cpp_Class/CMakeFiles/Cpp_Class.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Cpp_Class.
+# This may be replaced when dependencies are built.
---
+++ Cpp_Class/object_and_class/Cpp_Class.cpp
@@ -0,0 +1,12 @@
+﻿// Cpp_Class.cpp: 定义应用程序的入口点。
+//
+
+#include "Cpp_Class.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
