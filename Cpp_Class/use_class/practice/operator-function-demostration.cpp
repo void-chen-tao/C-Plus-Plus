e5e5595c8f49f2b1523cb8522916ad50b471d5e1
陈 涛 <1053949792@qq.com>
2022-05-30 18:25:12 +0800

    review use class

---
+++ Cpp_Class/use_class/practice/operator-function-demostration.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "operator-function-demostration.h"
+
+
+Time::Time()
+{
+    hours = minutes = 0;
+}
+
+Time::Time(int h, int m)
+{
+    hours = h;
+    minutes = m;
+}
+
+void Time::AddMin(int m)
+{
+    
+}
